551df775ee00881c308f2f9c92ad1bc8708a9438
Qing Tang <zucchini.tang@gmail.com>
2018-12-09 19:07:57 +0900

    Update train.py

--- train.py
+++ train.py
@@ -191,7 +191,7 @@ def train():
             update_vis_plot(iteration, loss_l.data[0], loss_c.data[0],
                             iter_plot, epoch_plot, 'append')
 
-        if iteration != 0 and iteration % 5000 == 0:
+        if iteration != 0 and iteration % 20 == 0:
             print('Saving state, iter:', iteration)
             torch.save(ssd_net.state_dict(), 'weights/ssd300_COCO_' +
                        repr(iteration) + '.pth')
